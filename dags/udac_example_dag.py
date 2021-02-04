cb369fe29e4baba3ec127059a04cb7f1fe50b9fb
Ian Logan <itlogan9@gmail.com>
2021-02-04 10:25:44 -0600

    Update template

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -6,30 +6,29 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
-
 default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2018, 11, 1),  # original datetime(2019, 1, 12)
-    'depends_on_past': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'email_on_retry': False,
-    'catchup_by_default': False
+    "owner": "Ian Logan",
+    "start_date": datetime(2018, 11, 1),
+    "end_date": datetime(2018, 11, 30),
+    "depends_on_past": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+    "email_on_retry": False,
+    "catchup_by_default": False
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG("s3_to_redshift_etl_dag",
           default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          #   schedule_interval='0 * * * *', # hourly (not optimal for log_data staging)
-          schedule_interval='@once'  # for testing
+          description="Load and transform data in Redshift with Airflow",
+          schedule_interval="@daily",
           )
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+# start
+start_operator = DummyOperator(task_id="Begin_execution",  dag=dag)
 
+# staging in database
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
+    task_id="Stage_events",
     dag=dag,
     table="staging_events",
     s3_key="log_data/{execution_date.year}/{execution_date.month}/{ds}-events.json",
@@ -37,54 +36,59 @@ stage_events_to_redshift = StageToRedshiftOperator(
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
+    task_id="Stage_songs",
     dag=dag,
     table="staging_songs",
     s3_key="song_data"
 )
 
+# load fact table
 load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
+    task_id="Load_songplays_fact_table",
     dag=dag,
     table="songplays",
     sql_select=SqlQueries.songplay_table_insert
 )
 
+# load dimension tables
 load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
+    task_id="Load_user_dim_table",
     dag=dag,
     table="users",
     sql_select=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
+    task_id="Load_song_dim_table",
     dag=dag,
     table="songs",
     sql_select=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
+    task_id="Load_artist_dim_table",
     dag=dag,
     table="artists",
     sql_select=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
+    task_id="Load_time_dim_table",
     dag=dag,
     table="time",
     sql_select=SqlQueries.time_table_insert
 )
 
+# data quality checking
 run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
+    task_id="Run_data_quality_checks",
     dag=dag
 )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+# end
+end_operator = DummyOperator(task_id="Stop_execution",  dag=dag)
 
+# dag dependencies
 start_operator >> stage_events_to_redshift
 start_operator >> stage_songs_to_redshift
 
